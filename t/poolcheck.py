78d75cbd3169b653fe7846603b6aee5a6eae9d42
Vincent Fu <vincent.fu@wdc.com>
2019-09-03 15:00:44 -0400

    t/poolcheck.py: documentation

--- t/poolcheck.py
+++ t/poolcheck.py
@@ -1,3 +1,32 @@
+#!/usr/bin/python2.7
+#
+# poolcheck.py
+#
+# This script processes smalloc debug output from the smalloc-debug branch.
+# The debug output looks like this:
+#
+# smalloc: size=112, pool=0, ptr=0x7f8a6201e6b0, caller=./fio-debug/fio(+0x19176) [0x5596b21e9176], free_blocks=523494
+# sfree: pool=0, ptr=0x7f8a6201e6b0, pool->free_blocks=523499
+#
+# The smalloc-debug branch logs each smalloc() and sfree() call to stderr and
+# provides information about the state of the relevant smalloc pools at the
+# time of each call. It can be used to check memory consumption and for memory
+# leaks.
+#
+# This script processes the debug output, auditing each smalloc() and sfree()
+# call, checking that buffers allocated are not already in use and that buffers
+# freed were previously allocated. It also periodically displays a list of
+# smalloc callers that hold storage space.
+#
+# At the very end this script prints out a list of the allocated buffers that
+# have not been acted upon by corresponding sfree() calls
+#
+#
+# Usage example:
+# ./fio .... |& tee fio-debug-01.log
+# python t/poolcheck.py fio-debug-01.log
+#
+
 import re
 import argparse
 
