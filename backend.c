b75ff25875f13d07e36d759834900098b3f90114
Ankit Kumar <ankit.kumar@samsung.com>
2022-12-12 18:45:07 -0500

    stats: support for printing of stats and eta for copy operation
    Signed-off-by: Ankit Kumar <ankit.kumar@samsung.com>
    Signed-off-by: Vincent Fu <vincent.fu@samsung.com>

--- backend.c
+++ backend.c
@@ -1924,6 +1924,8 @@ static void *thread_main(void *data)
 			update_runtime(td, elapsed_us, DDIR_WRITE);
 		if (td_trim(td) && td->io_bytes[DDIR_TRIM])
 			update_runtime(td, elapsed_us, DDIR_TRIM);
+		if (td_copy(td) && td->io_bytes[DDIR_COPY])
+			update_runtime(td, elapsed_us, DDIR_COPY);
 		fio_gettime(&td->start, NULL);
 		fio_sem_up(stat_sem);
 
@@ -2593,6 +2595,7 @@ int fio_backend(struct sk_out *sk_out)
 		setup_log(&agg_io_log[DDIR_READ], &p, "agg-read_bw.log");
 		setup_log(&agg_io_log[DDIR_WRITE], &p, "agg-write_bw.log");
 		setup_log(&agg_io_log[DDIR_TRIM], &p, "agg-trim_bw.log");
+		setup_log(&agg_io_log[DDIR_COPY], &p, "agg-copy_bw.log");
 	}
 
 	if (init_global_dedupe_working_set_seeds()) {
