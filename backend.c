vincentkfu/fio

commit 397f133880b4ef22e9fdb94f70f797c563fb40d6

fio: prevent OOM by not allocating buffer for TRIM range
Large TRIM ranges combined with high iodepth can cause memory usage
to spike and trigger Out Of Memory (OOM) errors. Since TRIM is a
metadata-only operation, it does not require a data payload buffer.
This patch adds a function to calculate the buffer size without DDIR_TRIM,
preventing unnecessary memory allocation.

Fixes: Issue #2056 https://github.com/axboe/fio/issues/2056
Signed-off-by: Dennis Chang <cherhungc@google.com>

backend.c
@@ -1467,7 +1467,10 @@ int init_io_u_buffers(struct thread_data *td)
 	char *p;
 
 	max_units = td->o.iodepth;
-	max_bs = td_max_bs(td);
+	if (td->trim_verify && td->o.trim_zero)
+		max_bs = td_max_bs(td);
+	else
+		max_bs = td_max_rw_bs(td);
 	min_write = td->o.min_bs[DDIR_WRITE];
 	td->orig_buffer_size = (unsigned long long) max_bs
 					* (unsigned long long) max_units;

fio.h
@@ -861,6 +861,14 @@ static inline bool should_check_rate(struct thread_data *td)
 	return (td->flags & TD_F_CHECK_RATE) != 0;
 }
 
+/*
+ * This function considers only read and write block sizes.
+ */
+static inline unsigned long long td_max_rw_bs(struct thread_data *td)
+{
+	return max(td->o.max_bs[DDIR_READ], td->o.max_bs[DDIR_WRITE]);
+}
+
 static inline unsigned long long td_max_bs(struct thread_data *td)
 {
 	unsigned long long max_bs;

commit 5d1160f3a19660c9a665f5866deaea5998949977

mem: adjust alloc size for [rand]trimwrite workloads
t/jobs/t0023.fio runs a randtrimwrite workload with bs=4k,4k,8k and has
been failiing from time to time especially on Windows. The unique
property of this job is that the trim block size specified in the 'bs'
option is greater than the write block size.

t0023.fio fails because fio is only considering the read and write block
sizes and not considering the trim block size when allocating data
buffers. Fio should be using 8k per queue depth to allocate data buffers
because each trim will be followed by a write of 8k even though the
write bs is set to 4k. This patch makes sure fio considers the size of
trim commands when allocating buffer memory for [rand]trimwrite
workloads.

However, the [rand]trimwrite check can return a false positive when reading an
iolog that contains both trim and write commands. So fio should only
trust the td_trimwrite() check when not reading from an iolog.

GitHub Copilot helped troubleshoot and diagnose this issue.

Fixes: 397f1338 ("fio: prevent OOM by not allocating buffer...")
Signed-off-by: Vincent Fu <vincent.fu@samsung.com>

backend.c
@@ -1574,7 +1574,26 @@ int init_io_u_buffers(struct thread_data *td)
 	char *p;
 
 	max_units = td->o.iodepth;
-	if (td->trim_verify && td->o.trim_zero)
+
+	/*
+	 * Trim operations do not involve data transfer. So we typically do not
+	 * have to account for trim sizes when allocating buffers. However,
+	 * there are exceptions:
+	 *
+	 * - if we are in trim verify mode we need a data buffer the same size
+	 *   as the trimmed range in order to confirm that the trimmed offset
+	 *   is all zeroes when we read it back
+	 * - if we are in [rand]trimwrite mode we need a data buffer the same
+	 *   size as the trimmed range in order to write data at the trimmed
+	 *   offset
+	 * - however, fio can mistakenly believe it is in [rand]trimwrite mode
+	 *   if it encounters trim and write operations when reading an iolog.
+	 *   So when replaying an iolog we do not need to account for trim
+	 *   sizes when allocating buffers even if the [rand]trimwrite check
+	 *   returns true because this is a false positive.
+	 */
+	if ((td->trim_verify && td->o.trim_zero) ||
+			(!(td->flags & TD_F_READ_IOLOG) && td_trimwrite(td)))
 		max_bs = td_max_bs(td);
 	else
 		max_bs = td_max_rw_bs(td);