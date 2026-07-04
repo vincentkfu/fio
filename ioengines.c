vincentkfu/fio

commit 7f3ecee2f6dd2aa40c0404a40d0406d9efc765f2

Track fsync/fdatasync/sync_file_range issue count
Also fix an issue where we didn't initialize the sync minimum
latency correctly, so it was always reported as 0.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

ioengines.c
@@ -357,7 +357,7 @@ int td_io_queue(struct thread_data *td, struct io_u *io_u)
 	}
 
 	if (ret == FIO_Q_COMPLETED) {
-		if (ddir_rw(io_u->ddir)) {
+		if (ddir_rw(io_u->ddir) || ddir_sync(io_u->ddir)) {
 			io_u_mark_depth(td, 1);
 			td->ts.total_io_u[io_u->ddir]++;
 		}
@@ -366,7 +366,7 @@ int td_io_queue(struct thread_data *td, struct io_u *io_u)
 
 		td->io_u_queued++;
 
-		if (ddir_rw(io_u->ddir))
+		if (ddir_rw(io_u->ddir) || ddir_sync(io_u->ddir))
 			td->ts.total_io_u[io_u->ddir]++;
 
 		if (td->io_u_queued >= td->o.iodepth_batch) {

init.c
@@ -1481,6 +1481,7 @@ static int add_job(struct thread_data *td, const char *jobname, int job_add_num,
 		td->ts.bw_stat[i].min_val = ULONG_MAX;
 		td->ts.iops_stat[i].min_val = ULONG_MAX;
 	}
+	td->ts.sync_stat.min_val = ULONG_MAX;
 	td->ddir_seq_nr = o->ddir_seq_nr;
 
 	if ((o->stonewall || o->new_group) && prev_group_jobs) {

io_ddir.h
@@ -5,8 +5,11 @@ enum fio_ddir {
 	DDIR_READ = 0,
 	DDIR_WRITE = 1,
 	DDIR_TRIM = 2,
-	DDIR_RWDIR_CNT = 3,
 	DDIR_SYNC = 3,
+
+	DDIR_RWDIR_CNT = 3,
+	DDIR_RWDIR_SYNC_CNT = 4,
+
 	DDIR_DATASYNC,
 	DDIR_SYNC_FILE_RANGE,
 	DDIR_WAIT,

stat.c
@@ -854,12 +854,13 @@ static void show_thread_status_normal(struct thread_stat *ts,
 					io_u_dist[1], io_u_dist[2],
 					io_u_dist[3], io_u_dist[4],
 					io_u_dist[5], io_u_dist[6]);
-	log_buf(out, "     issued rwt: total=%llu,%llu,%llu,"
-				 " short=%llu,%llu,%llu,"
-				 " dropped=%llu,%llu,%llu\n",
+	log_buf(out, "     issued rwts: total=%llu,%llu,%llu,%llu"
+				 " short=%llu,%llu,%llu,0"
+				 " dropped=%llu,%llu,%llu,0\n",
 					(unsigned long long) ts->total_io_u[0],
 					(unsigned long long) ts->total_io_u[1],
 					(unsigned long long) ts->total_io_u[2],
+					(unsigned long long) ts->total_io_u[3],
 					(unsigned long long) ts->short_io_u[0],
 					(unsigned long long) ts->short_io_u[1],
 					(unsigned long long) ts->short_io_u[2],
@@ -1609,6 +1610,8 @@ void sum_thread_stats(struct thread_stat *dst, struct thread_stat *src,
 		}
 	}
 
+	dst->total_io_u[DDIR_SYNC] += src->total_io_u[DDIR_SYNC];
+
 	for (k = 0; k < DDIR_RWDIR_CNT; k++) {
 		int m;
 
@@ -1647,6 +1650,7 @@ void init_thread_stat(struct thread_stat *ts)
 		ts->bw_stat[j].min_val = -1UL;
 		ts->iops_stat[j].min_val = -1UL;
 	}
+	ts->sync_stat.min_val = -1UL;
 	ts->groupid = -1;
 }
 
@@ -2286,6 +2290,8 @@ void reset_io_stats(struct thread_data *td)
 				ts->io_u_sync_plat[j] = 0;
 		}
 	}
+
+	ts->total_io_u[DDIR_SYNC] = 0;
 
 	for (i = 0; i < FIO_IO_U_MAP_NR; i++) {
 		ts->io_u_map[i] = 0;

stat.h
@@ -192,7 +192,7 @@ struct thread_stat {
 	uint32_t io_u_sync_plat[FIO_IO_U_PLAT_NR];
 	uint32_t pad;
 
-	uint64_t total_io_u[DDIR_RWDIR_CNT];
+	uint64_t total_io_u[DDIR_RWDIR_SYNC_CNT];
 	uint64_t short_io_u[DDIR_RWDIR_CNT];
 	uint64_t drop_io_u[DDIR_RWDIR_CNT];
 	uint64_t total_submit;

commit af511749958d942bebe5511f26c8e6977952e360

ioengines: record all sync io_u completions under DDIR_SYNC in total_io_u
When queuing or completing synchronous I/O operations
(`ddir_sync(io_u->ddir)` is true), increment `td->ts.total_io_u[DDIR_SYNC]`
instead of `td->ts.total_io_u[io_u->ddir]`.

The `total_io_u` array is sized by `DDIR_RWDIR_SYNC_CNT` (4 elements: read,
write, trim, sync). Indexing directly with `io_u->ddir` for other
synchronous I/O types such as `DDIR_DATASYNC`, `DDIR_SYNC_FILE_RANGE`, or
`DDIR_SYNCFS` resulted in out-of-bounds array accesses. Aggregating all
synchronous I/O completion counts under `DDIR_SYNC` prevents memory
corruption and accurately tracks total sync I/O units.

This patch addresses the following Coverity complaint:

*** CID 647239:         Memory - corruptions  (OVERRUN)
/backend.c: 314             in fio_syncfs()
308     		if (fio_open_fs(td, fm)) {
309     			log_err("open %s for syncfs failed\n", fm->base);
310     			err = -1;
311     			continue;
312     		}
313
>>>     CID 647239:         Memory - corruptions  (OVERRUN)
>>>     Overrunning callee's array of size 4 by passing argument "DDIR_SYNCFS" in call to "fio_io_sync".
314     		if (fio_io_sync(td, fm->f, DDIR_SYNCFS)) {
315     			log_err("syncfs %s failed\n", fm->base);
316     			err = -1;
317     			continue;
318     		}
319

Fixes: 7f3ecee2f6dd ("Track fsync/fdatasync/sync_file_range issue count")
Signed-off-by: Bart Van Assche <bvanassche@acm.org>

ioengines.c
@@ -441,19 +441,22 @@ enum fio_q_status td_io_queue(struct thread_data *td, struct io_u *io_u)
 	}
 
 	if (ret == FIO_Q_COMPLETED) {
-		if (ddir_rw(io_u->ddir) ||
-		    (ddir_sync(io_u->ddir) && td->runstate != TD_FSYNCING)) {
+		if (ddir_rw(io_u->ddir)) {
 			io_u_mark_depth(td, 1);
 			td->ts.total_io_u[io_u->ddir]++;
+		} else if (ddir_sync(io_u->ddir) && td->runstate != TD_FSYNCING) {
+			io_u_mark_depth(td, 1);
+			td->ts.total_io_u[DDIR_SYNC]++;
 		}
 
 		td->last_ddir_issued = ddir;
 	} else if (ret == FIO_Q_QUEUED) {
 		td->io_u_queued++;
 
-		if (ddir_rw(io_u->ddir) ||
-		    (ddir_sync(io_u->ddir) && td->runstate != TD_FSYNCING))
+		if (ddir_rw(io_u->ddir))
 			td->ts.total_io_u[io_u->ddir]++;
+		else if (ddir_sync(io_u->ddir) && td->runstate != TD_FSYNCING)
+			td->ts.total_io_u[DDIR_SYNC]++;
 
 		if (td->io_u_queued >= td->o.iodepth_batch)
 			td_io_commit(td);