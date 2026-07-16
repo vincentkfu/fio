63df4628b75530bb95ee952a6572f76e44993df9
Vincent Fu <vincent.fu@samsung.com>
2026-07-16 11:59:00 -0400

    fio: fix const char * warning
    fio_show_ioengine_help takes a const char * but may modify the
    associated string. This triggers a warning in newer gcc versions. Avoid
    this warning by modifying a copy of the original string.
    
    Signed-off-by: Vincent Fu <vincent.fu@samsung.com>

--- ioengines.c
+++ ioengines.c
@@ -711,6 +711,7 @@ int fio_show_ioengine_help(const char *engine)
 	struct thread_data td;
 	struct ioengine_ops *io_ops;
 	char *sep;
+	char *engine_name;
 	int ret = 1;
 
 	memset(&td, 0, sizeof(struct thread_data));
@@ -724,17 +725,25 @@ int fio_show_ioengine_help(const char *engine)
 		}
 		return 0;
 	}
-	sep = strchr(engine, ',');
+
+	engine_name = strdup(engine);
+	if (!engine_name) {
+		log_err("fio: out of memory\n");
+		return 1;
+	}
+
+	sep = strchr(engine_name, ',');
 	if (sep) {
 		*sep = 0;
 		sep++;
 	}
 
-	td.o.ioengine = (char *)engine;
+	td.o.ioengine = engine_name;
 	td.io_ops = load_ioengine(&td);
 
 	if (!td.io_ops) {
-		log_info("IO engine %s not found\n", engine);
+		log_info("IO engine %s not found\n", engine_name);
+		free(engine_name);
 		return 1;
 	}
 
@@ -744,5 +753,6 @@ int fio_show_ioengine_help(const char *engine)
 		log_info("IO engine %s has no options\n", td.io_ops->name);
 
 	free_ioengine(&td);
+	free(engine_name);
 	return ret;
 }
